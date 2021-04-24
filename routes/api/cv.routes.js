67039889fc2d0eb435fdde56f9cc68b7cda1c4d3
Jean <jeanastacio31@gmail.com>
2021-04-23 20:33:43 -0400

    Get cv working

--- routes/api/cv.routes.js
+++ routes/api/cv.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+var cvJson = require("../../myJsonData")
 
 // 1) GET
 router.get("/", (req, resp) => {
-    resp.send("Welcome to my app");
+    resp.json(cvJson);
 })
 // 2) POST
 // 3) PUT
