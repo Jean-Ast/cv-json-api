ace8ff78ef15d3936aea255b4ef35bb23bddd71b
Jean <jeanastacio31@gmail.com>
2021-04-25 15:32:13 -0400

    DELETE Auth done

--- routes/api/cv.routes.js
+++ routes/api/cv.routes.js
@@ -5,8 +5,8 @@ var cvJson = require("../../myJsonData");
 // User Info to login
 const userInfo = {
   username: "jean",
-  password: "pass"
-}
+  password: "pass",
+};
 
 // 1) GET
 router.get("/", (req, resp) => {
@@ -18,7 +18,7 @@ router.get("/:session", (req, resp) => {
   // resp.send(req.params);
   let mySession = req.params.session;
   resp.send(cvJson[mySession]);
-})
+});
 
 // 1.2 GET Ruta navegable => cv.session.subSession
 router.get("/:session/subsession/:mysubsession", (req, resp) => {
@@ -30,13 +30,15 @@ router.get("/:session/subsession/:mysubsession", (req, resp) => {
   let newcvJson = cvJson[_mysession];
   let completeSubsession = newcvJson[_mysubsession];
   // Send the new cvJson subsession
-  resp.send(completeSubsession)
-})
+  resp.send(completeSubsession);
+});
 
 // 2) POST a new session
 router.post("/", (req, resp) => {
   if (req.headers.authorization === undefined) {
-    resp.status(401).send("Please provide basic auth in headers with base64 encoding");
+    resp
+      .status(401)
+      .send("Please provide basic auth in headers with base64 encoding");
   } else {
     // Encode request value headers
     var encodedReq = req.headers.authorization.split(" ")[1];
@@ -46,7 +48,10 @@ router.post("/", (req, resp) => {
     var password = decodeReq.split(":")[1];
 
     // Valide credentials with correct userName and correct password
-    if (userInfo.username.toLowerCase() === userName.toLowerCase() && userInfo.password === password) {
+    if (
+      userInfo.username.toLowerCase() === userName.toLowerCase() &&
+      userInfo.password === password
+    ) {
       try {
         if (req.body != null) {
           var newSession = req.body;
@@ -57,14 +62,20 @@ router.post("/", (req, resp) => {
       }
       resp.send(cvJson);
     } else {
-      resp.status(401).send("Invalid data provided. Please check your username and password, then try again")
+      resp
+        .status(401)
+        .send(
+          "Invalid data provided. Please check your username and password, then try again"
+        );
     }
   }
 });
 // 3) PUT
 router.put("/:session", (req, resp) => {
   if (req.headers.authorization === undefined) {
-    resp.status(401).send("Please provide basic auth in headers with base64 encoding");
+    resp
+      .status(401)
+      .send("Please provide basic auth in headers with base64 encoding");
   } else {
     // Encode request value headers
     var encodedReq = req.headers.authorization.split(" ")[1];
@@ -74,8 +85,10 @@ router.put("/:session", (req, resp) => {
     var password = decodeReq.split(":")[1];
 
     // Valide credentials with correct userName and correct password
-    if (userInfo.username.toLowerCase() === userName.toLowerCase() && userInfo.password === password) {
-
+    if (
+      userInfo.username.toLowerCase() === userName.toLowerCase() &&
+      userInfo.password === password
+    ) {
       // Find session
       const sessiontoUpd = req.params.session;
       var result = cvJson[sessiontoUpd];
@@ -91,20 +104,40 @@ router.put("/:session", (req, resp) => {
       });
       resp.send(cvJson);
     }
-}});
+  }
+});
 // 4) DELETE
 router.delete("/:session", (req, resp) => {
-  // Find session
-  const sessiontoUpd = req.params.session;
-  var result = cvJson[sessiontoUpd];
-  // Iterate cvJson
-  Object.keys(cvJson).forEach((key) => {
-    if (cvJson[key] === result) {
-      // Delete session
-      delete cvJson[key];
+  if (req.headers.authorization === undefined) {
+    resp
+      .status(401)
+      .send("Please provide basic auth in headers with base64 encoding");
+  } else {
+    // Encode request value headers
+    var encodedReq = req.headers.authorization.split(" ")[1];
+    // Decode request value headers
+    var decodeReq = new Buffer(encodedReq, "base64").toString();
+    var userName = decodeReq.split(":")[0];
+    var password = decodeReq.split(":")[1];
+
+    // Valide credentials with correct userName and correct password
+    if (
+      userInfo.username.toLowerCase() === userName.toLowerCase() &&
+      userInfo.password === password
+    ) {
+      // Find session
+      const sessiontoUpd = req.params.session;
+      var result = cvJson[sessiontoUpd];
+      // Iterate cvJson
+      Object.keys(cvJson).forEach((key) => {
+        if (cvJson[key] === result) {
+          // Delete session
+          delete cvJson[key];
+        }
+      });
+      resp.send(cvJson);
     }
-  });
-  resp.send(cvJson);
+  }
 });
 
 module.exports = router;
