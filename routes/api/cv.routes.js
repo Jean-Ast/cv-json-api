fba1d5ea42a1b883ca0fd544566ae2bd7c836382
Jean <jeanastacio31@gmail.com>
2021-04-25 15:28:04 -0400

    POST auth done

--- routes/api/cv.routes.js
+++ routes/api/cv.routes.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 var cvJson = require("../../myJsonData");
 
+// User Info to login
+const userInfo = {
+  username: "jean",
+  password: "pass"
+}
+
 // 1) GET
 router.get("/", (req, resp) => {
   resp.send(cvJson);
@@ -26,17 +32,34 @@ router.get("/:session/subsession/:mysubsession", (req, resp) => {
   // Send the new cvJson subsession
   resp.send(completeSubsession)
 })
+
 // 2) POST a new session
 router.post("/", (req, resp) => {
-  try {
-    if (req.body != null) {
-      var newSession = req.body;
-      cvJson = { cvJson, ...newSession };
+  if (req.headers.authorization === undefined) {
+    resp.status(401).send("Please provide basic auth in headers with base64 encoding");
+  } else {
+    // Encode request value headers
+    var encodedReq = req.headers.authorization.split(" ")[1];
+    // Decode request value headers
+    var decodeReq = new Buffer(encodedReq, "base64").toString();
+    var userName = decodeReq.split(":")[0];
+    var password = decodeReq.split(":")[1];
+
+    // Valide credentials with correct userName and correct password
+    if (userInfo.username.toLowerCase() === userName.toLowerCase() && userInfo.password === password) {
+      try {
+        if (req.body != null) {
+          var newSession = req.body;
+          cvJson = { cvJson, ...newSession };
+        }
+      } catch (err) {
+        console.log(err);
+      }
+      resp.send(cvJson);
+    } else {
+      resp.status(401).send("Invalid data provided. Please check your username and password, then try again")
     }
-  } catch (err) {
-    console.log(err);
   }
-  resp.send(cvJson);
 });
 // 3) PUT
 router.put("/:session", (req, resp) => {
